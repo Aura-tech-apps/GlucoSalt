147e48454ca8a27040f6e33e73190e4a44e88b4d
Aura-tech-apps <ferramentas@auramatrix.com.br>
2025-11-13 13:16:25 -0300

    d

--- functions/klm.js
+++ functions/klm.js
@@ -1,4 +1,3 @@
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  return fetch(new URL("/front/lead1/vsl-27.html", url).toString(), context.request);
+  return context.env.ASSETS.fetch("/front/lead1/vsl-27.html", context.request);
 }
