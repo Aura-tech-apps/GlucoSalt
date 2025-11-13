6bf92f75a931e106275af8757a57c13ccb5e1532
Aura-tech-apps <ferramentas@auramatrix.com.br>
2025-11-13 13:41:06 -0300

    correções

--- functions/nop.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.pathname = "/front/lead1/vsl-37.html";
-  return fetch(url, context.request);
-}
--- functions/yuo.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  return fetch(new URL("/presell/presell.html", url).toString(), context.request);
-}
