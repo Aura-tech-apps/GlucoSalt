732489e74b6ba4d3e94b96d17eca2fd2b1a7b9af
Aura-tech-apps <ferramentas@auramatrix.com.br>
2025-11-13 13:55:35 -0300

    Backredirect

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -3,25 +3,45 @@ const ROUTE_MAP = {
   // PRESELL
   '/yuo': '/presell/presell.html',
   
-  // LEAD 1
+  // LEAD 1 - VSL Pages
   '/klm': '/front/lead1/vsl-27.html',
   '/nop': '/front/lead1/vsl-37.html',
   '/ntg': '/front/lead1/vsl-47.html',
   
-  // LEAD 2
+  // LEAD 1 - Backredirect Pages
+  '/klm-b': '/backredirect/lead1/var-27.html',
+  '/nop-b': '/backredirect/lead1/var-37.html',
+  '/ntg-b': '/backredirect/lead1/var-47.html',
+  
+  // LEAD 2 - VSL Pages
   '/efg': '/front/lead2/vsl-27.html',
   '/hij': '/front/lead2/vsl-37.html',
   '/bvc': '/front/lead2/vsl-47.html',
   
-  // LEAD 3
+  // LEAD 2 - Backredirect Pages
+  '/efg-b': '/backredirect/lead2/var-27.html',
+  '/hij-b': '/backredirect/lead2/var-37.html',
+  '/bvc-b': '/backredirect/lead2/var-47.html',
+  
+  // LEAD 3 - VSL Pages
   '/yza': '/front/lead3/vsl-27.html',
   '/bcd': '/front/lead3/vsl-37.html',
   '/fas': '/front/lead3/vsl-47.html',
   
-  // LEAD 4
+  // LEAD 3 - Backredirect Pages
+  '/yza-b': '/backredirect/lead3/var-27.html',
+  '/bcd-b': '/backredirect/lead3/var-37.html',
+  '/fas-b': '/backredirect/lead3/var-47.html',
+  
+  // LEAD 4 - VSL Pages
   '/hgx': '/front/lead4/vsl-27.html',
   '/jjg': '/front/lead4/vsl-37.html',
   '/ewq': '/front/lead4/vsl-47.html',
+  
+  // LEAD 4 - Backredirect Pages
+  '/hgx-b': '/backredirect/lead4/var-27.html',
+  '/jjg-b': '/backredirect/lead4/var-37.html',
+  '/ewq-b': '/backredirect/lead4/var-47.html',
 };
 
 export async function onRequest(context) {
